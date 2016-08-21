86040fee27dfdd497bc0a449e9b6998a225e0dc3
broskh <alessio.scheri@gmail.com>
2016-08-21 23:46:40 +0200

    Aggiunto modulo e interfaccia per la gestione delle impostazioni

---
+++ gestione_impostazioni.cc
@@ -0,0 +1,83 @@
+/*
+ * File contenente il modulo di salvataggio/caricamento delle impostazioni.
+ */
+
+#include <cstring>
+#include "struttura_dati.h"
+#include "gestione_impostazioni.h"
+
+const unsigned int MAX_STRING = 15;
+
+//INIZIO MODULO
+
+
+bool caricaImpostazioni (impostazioni &impo, const char file [])
+{
+	bool musica_trov = false, eff_audio_trov = false, mod_grafica_trov = false, vite_iniz_trov = false;
+	bool musica, eff_audio;
+	grafica mod_grafica;
+	unsigned int vite_iniziali;
+	char voice [MAX_STRING];
+	ifstream f (file) ;
+    	if (!f) {
+		cerr<<"Errore nel caricamento del file "<<file<<endl;
+		return false ;
+    	}
+	while (f>>voice)
+	{
+		f.ignore (2, '=');
+		if (strcmp(voice, "musica") == 0)
+		{
+			f>>musica;
+			musica_trov = true;
+		}
+		else if (strcmp(voice, "effetti_audio") == 0)
+		{
+			f>>eff_audio;
+			eff_audio_trov = true;
+		}
+		else if (strcmp(voice, "grafica") == 0)
+		{
+			int grafica_int;
+			f>>grafica_int;
+			mod_grafica = static_cast <grafica> (grafica_int);
+			mod_grafica_trov = true;
+		}
+		else if (strcmp(voice, "vite_iniziali") == 0)
+		{
+			f>>vite_iniziali;
+			vite_iniz_trov = true;
+		}
+    	}
+	if (musica_trov || eff_audio_trov || mod_grafica_trov || vite_iniz_trov)
+	{
+		inizializzaImpostazioni (impo, musica, eff_audio, mod_grafica, vite_iniziali);
+		return true;
+	}
+	cerr<<"File di configurazione incompleto"<<endl;
+	return false;
+}
+
+void salvaImpostazioni (impostazioni impo, const char file [])
+{
+	ofstream f(file) ;
+	f<<"musica = "<<impo.musica<<endl;
+	f<<"effetti_audio = "<<impo.eff_audio<<endl;
+	f<<"grafica = "<<impo.mod_grafica<<endl;
+	f<<"vite_iniziali = "<<impo.vite_iniziali<<endl;
+}
+
+void inizializzaImpostazioni (impostazioni &impo, bool musica, bool effetti_audio, grafica modalita_grafica, unsigned int vite_iniziali)
+{
+	impo.musica = musica;
+	impo.eff_audio = effetti_audio;
+	impo.mod_grafica = modalita_grafica;
+	impo.vite_iniziali = vite_iniziali;
+}
+
+void impostaValoraImpostazioniDefault (impostazioni &impo)
+{
+	inizializzaImpostazioni (impo, DEFAULT_MUSICA, DEFAULT_EFFETTI_AUDIO, DEFAULT_MODALITA_GRAFICA, DEFAULT_VITE_INIZIALI);
+}
+
+//FINE MODULO
---
+++ gestione_impostazioni.h
@@ -0,0 +1,50 @@
+/*
+ * Header file contenente l'interfaccia del modulo di salvataggio/caricamento delle impostazioni.
+ */
+
+#include <fstream>
+
+//INIZIO INTERFACCIA
+
+const bool DEFAULT_MUSICA = true; /*Valore di default del campo musica.*/
+const bool DEFAULT_EFFETTI_AUDIO = true; /*Valore di default del campo effetti_audio.*/
+const grafica DEFAULT_MODALITA_GRAFICA = mono_bianco; /*Valore di default del campo mod_grafica.*/
+const unsigned int DEFAULT_VITE_INIZIALI = 3; /*Valore di default del campo vite_iniziali.*/
+
+/*
+ * Carica da un file di configurazione le impostazioni e le memorizza in un'apposita struttura.
+ * Ritorna false nel caso in cui il file passato come parametro non esiste.
+ * 
+ * impo Struttura "impostazioni" nella quale memorizzare i valori.
+ * file File di configurazione dal quale caricare le impostazioni.
+ */
+bool caricaImpostazioni (impostazioni &impo, const char file []);
+
+/*
+ * Salva le impostazioni su un file di configurazione.
+ *
+ * impo Struttura "impostazioni" che contiene le impostazioni da salvare nel file di configurazione. 
+ * file File di configurazione nel quale salvare le impostazioni.
+ */
+void salvaImpostazioni (impostazioni impo, const char file []);
+
+/*
+ * Memorizza in una struttura "impostazioni" i valori passati per parametro.
+ *
+ * impo Struttura "impostazioni" nella quale verranno memorizzati i valori.
+ * musica Valore booleano da assegnare al campo "musica".
+ * effetti_audio Valore booleano da assegnare al campo "eff_audio".
+ * modalita_grafica Valore "grafica" da assegnare al campo "mod_grafica".
+ * vite_iniziali Valore intero da assegnare al campo "vite_iniziali".
+ */
+void inizializzaImpostazioni (impostazioni &impo, bool musica, bool effetti_audio, grafica modalita_grafica, unsigned int vite_iniziali);
+
+/*
+ * Ritorna una struttura "impostazioni" contenente i valori di default.
+ *
+ * impo Struttura "impostazioni" nella quale vengono settati i valori di default.
+ */
+void impostaValoraImpostazioniDefault (impostazioni &impo);
+
+
+//FINE INTERFACCIA
