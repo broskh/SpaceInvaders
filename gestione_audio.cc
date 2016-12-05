82e089da4054c1e86258c9d9d6def07a0d8e4bdd
broskh <alessio.scheri@gmail.com>
2016-12-05 15:38:07 +0100

    Migliorato leggibilita' e sistemata documentazione del modulo per la gestione dell'audio.

--- gestione_audio.cc
+++ gestione_audio.cc
@@ -1,5 +1,5 @@
 /*
- * File contenente il modulo di gestione dell'audio.
+ * File contenente il modulo per la gestione dell'audio.
  */
 
 #include <allegro5/allegro_audio.h>
@@ -7,63 +7,56 @@
 #include "gestione_audio.h"
 
 //INIZIO VARIABILI DI MODULO
-static ALLEGRO_SAMPLE *musica_principale = NULL;
-static ALLEGRO_SAMPLE *musica_sottofondo_ondata = NULL;
-static ALLEGRO_SAMPLE *suono_esplosione_carro_armato = NULL;
-static ALLEGRO_SAMPLE *suono_esplosione_alieno = NULL;
-static ALLEGRO_SAMPLE *suono_esplosione_navicella_misteriosa = NULL;
-static ALLEGRO_SAMPLE *suono_sparo_carro_armato = NULL;
-static ALLEGRO_SAMPLE *suono_navicella_misteriosa = NULL;
+static ALLEGRO_SAMPLE *musica_principale = NULL; /**<Musica principale.*/
+static ALLEGRO_SAMPLE *musica_sottofondo_ondata = NULL; /**<Musica di sottofondo all'ondata di alieni in movimento.*/
+static ALLEGRO_SAMPLE *suono_esplosione_alieno = NULL; /**<Suono dell'esplosione di un alieno.*/
+static ALLEGRO_SAMPLE *suono_esplosione_carro_armato = NULL; /**<Suono dell'esplosione del carro armato.*/
+static ALLEGRO_SAMPLE *suono_esplosione_navicella_misteriosa = NULL; /**<Suono dell'esplosione della navicella misteriosa.*/
+static ALLEGRO_SAMPLE *suono_navicella_misteriosa = NULL; /**<Suono della navicella msiteriosa in movimento.*/
+static ALLEGRO_SAMPLE *suono_sparo_carro_armato = NULL; /**<Suono dello sparo del carro armato.*/
 
-static ALLEGRO_SAMPLE_INSTANCE *istanza_sottofondo_ondata = NULL;
-static ALLEGRO_SAMPLE_INSTANCE *istanza_esplosione_carro_armato = NULL;
-static ALLEGRO_SAMPLE_INSTANCE *istanza_esplosione_alieno = NULL;
-static ALLEGRO_SAMPLE_INSTANCE *istanza_esplosione_navicella_misteriosa = NULL;
-static ALLEGRO_SAMPLE_INSTANCE *istanza_sparo_carro_armato = NULL;
-static ALLEGRO_SAMPLE_INSTANCE *istanza_navicella_misteriosa = NULL;
+static ALLEGRO_SAMPLE_INSTANCE *istanza_esplosione_alieno = NULL; /**<Istanza del suono di esplosione di un alieno.*/
+static ALLEGRO_SAMPLE_INSTANCE *istanza_esplosione_carro_armato = NULL; /**<Istanza del suono di esplosione del carro armato.*/
+static ALLEGRO_SAMPLE_INSTANCE *istanza_esplosione_navicella_misteriosa = NULL; /**<Istanza del suono di esplosione della navicella misteriosa.*/
+static ALLEGRO_SAMPLE_INSTANCE *istanza_navicella_misteriosa = NULL; /**<Istanza del suono della navicella misteriosa in movimento.*/
+static ALLEGRO_SAMPLE_INSTANCE *istanza_sottofondo_ondata = NULL; /**<Istanza della musica di sottofondo all'ondata di alieni in movimento.*/
+static ALLEGRO_SAMPLE_INSTANCE *istanza_sparo_carro_armato = NULL; /**<Istanza del suono dello sparo del carro armato.*/
 //FINE VARIABILI DI MODULO
 
 //INIZIO MODULO
-void inizializzaAudio ()
+void avviaMusicaOndata ()
 {
-	assert (al_install_audio());
-	assert (al_init_acodec_addon());
-	assert (al_reserve_samples(true));
+	al_play_sample_instance(istanza_sottofondo_ondata);
+}
 
-	musica_principale = al_load_sample (FILE_MUSICA_PRINCIPALE);
-	assert (musica_principale);
+void avviaMusicaPrincipale ()
+{
+	al_play_sample(musica_principale, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
+}
 
-	musica_sottofondo_ondata = al_load_sample (FILE_MUSICA_SOTTOFONDO_ONDATA);
-	assert (musica_sottofondo_ondata);
-	istanza_sottofondo_ondata = al_create_sample_instance (musica_sottofondo_ondata);
-	al_attach_sample_instance_to_mixer(istanza_sottofondo_ondata, al_get_default_mixer());
-	al_set_sample_instance_playmode(istanza_sottofondo_ondata, ALLEGRO_PLAYMODE_LOOP);
+void avviaSuonoEsplosioneAlieno ()
+{
+	al_play_sample_instance(istanza_esplosione_alieno);
+}
 
-	suono_esplosione_carro_armato = al_load_sample (FILE_SUONO_ESPLOSIONE_CARRO_ARMATO);
-	assert (suono_esplosione_carro_armato);
-	istanza_esplosione_carro_armato = al_create_sample_instance (suono_esplosione_carro_armato);
-	al_attach_sample_instance_to_mixer(istanza_esplosione_carro_armato, al_get_default_mixer());
+void avviaSuonoEsplosioneCarroArmato ()
+{
+	al_play_sample_instance(istanza_esplosione_carro_armato);
+}
 
-	suono_esplosione_alieno = al_load_sample (FILE_SUONO_ESPLOSIONE_ALIENO);
-	assert (suono_esplosione_alieno);
-	istanza_esplosione_alieno = al_create_sample_instance (suono_esplosione_alieno);
-	al_attach_sample_instance_to_mixer(istanza_esplosione_alieno, al_get_default_mixer());
+void avviaSuonoEsplosioneNavicellaMisteriosa ()
+{
+	al_play_sample_instance(istanza_esplosione_navicella_misteriosa);
+}
 
-	suono_esplosione_navicella_misteriosa = al_load_sample (FILE_SUONO_ESPLOSIONE_NAVICELLA_MISTERIOSA);
-	assert (suono_esplosione_navicella_misteriosa);
-	istanza_esplosione_navicella_misteriosa = al_create_sample_instance (suono_esplosione_navicella_misteriosa);
-	al_attach_sample_instance_to_mixer(istanza_esplosione_navicella_misteriosa, al_get_default_mixer());
+void avviaSuonoNavicellaMisteriosa ()
+{
+	al_play_sample_instance(istanza_navicella_misteriosa);
+}
 
-	suono_sparo_carro_armato = al_load_sample (FILE_SUONO_SPARO_CARRO_ARMATO);
-	assert (suono_sparo_carro_armato);
-	istanza_sparo_carro_armato = al_create_sample_instance (suono_sparo_carro_armato);
-	al_attach_sample_instance_to_mixer(istanza_sparo_carro_armato, al_get_default_mixer());
-
-	suono_navicella_misteriosa = al_load_sample (FILE_SUONO_NAVICELLA_MISTERIOSA);
-	assert (suono_navicella_misteriosa);
-	istanza_navicella_misteriosa = al_create_sample_instance (suono_navicella_misteriosa);
-	al_attach_sample_instance_to_mixer(istanza_navicella_misteriosa, al_get_default_mixer());
-	al_set_sample_instance_playmode(istanza_navicella_misteriosa, ALLEGRO_PLAYMODE_LOOP);
+void avviaSuonoSparoCarroArmato ()
+{
+	al_play_sample_instance(istanza_sparo_carro_armato);
 }
 
 void distruggiAudio ()
@@ -83,9 +76,9 @@ void distruggiAudio ()
 	al_destroy_sample_instance (istanza_navicella_misteriosa);
 }
 
-void avviaMusicaPrincipale ()
+void fermaMusicaOndata ()
 {
-	al_play_sample(musica_principale, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
+	al_stop_sample_instance(istanza_sottofondo_ondata);
 }
 
 void fermaMusicaPrincipale ()
@@ -93,44 +86,57 @@ void fermaMusicaPrincipale ()
 	al_stop_samples ();
 }
 
-void avviaMusicaOndata ()
-{
-	al_play_sample_instance(istanza_sottofondo_ondata);
-}
-
-void fermaMusicaOndata ()
-{
-	al_stop_sample_instance(istanza_sottofondo_ondata);
-}
-
-void avviaSuonoNavicellaMisteriosa ()
-{
-	al_play_sample_instance(istanza_navicella_misteriosa);
-}
-
 void fermaSuonoNavicellaMisteriosa ()
 {
 	al_stop_sample_instance(istanza_navicella_misteriosa);
 }
 
-void avviaSuonoSparoCarroArmato ()
+void inizializzaAudio ()
 {
-	al_play_sample_instance(istanza_sparo_carro_armato);
-}
+	assert (al_install_audio());
+	assert (al_init_acodec_addon());
+	assert (al_reserve_samples(true));
 
-void avviaSuonoEsplosioneCarroArmato ()
-{
-	al_play_sample_instance(istanza_esplosione_carro_armato);
-}
+	musica_principale = al_load_sample (FILE_MUSICA_PRINCIPALE);
+	assert (musica_principale);
 
-void avviaSuonoEsplosioneAlieno ()
-{
-	al_play_sample_instance(istanza_esplosione_alieno);
-}
+	musica_sottofondo_ondata = al_load_sample (FILE_MUSICA_SOTTOFONDO_ONDATA);
+	assert (musica_sottofondo_ondata);
+	istanza_sottofondo_ondata = al_create_sample_instance (musica_sottofondo_ondata);
+	assert (istanza_sottofondo_ondata);
+	al_attach_sample_instance_to_mixer(istanza_sottofondo_ondata, al_get_default_mixer());
+	al_set_sample_instance_playmode(istanza_sottofondo_ondata, ALLEGRO_PLAYMODE_LOOP);
 
-void avviaSuonoEsplosioneNavicellaMisteriosa ()
-{
-	al_play_sample_instance(istanza_esplosione_navicella_misteriosa);
+	suono_esplosione_carro_armato = al_load_sample (FILE_SUONO_ESPLOSIONE_CARRO_ARMATO);
+	assert (suono_esplosione_carro_armato);
+	istanza_esplosione_carro_armato = al_create_sample_instance (suono_esplosione_carro_armato);
+	assert (istanza_esplosione_carro_armato);
+	al_attach_sample_instance_to_mixer(istanza_esplosione_carro_armato, al_get_default_mixer());
+
+	suono_esplosione_alieno = al_load_sample (FILE_SUONO_ESPLOSIONE_ALIENO);
+	assert (suono_esplosione_alieno);
+	istanza_esplosione_alieno = al_create_sample_instance (suono_esplosione_alieno);
+	assert (istanza_esplosione_alieno);
+	al_attach_sample_instance_to_mixer(istanza_esplosione_alieno, al_get_default_mixer());
+
+	suono_esplosione_navicella_misteriosa = al_load_sample (FILE_SUONO_ESPLOSIONE_NAVICELLA_MISTERIOSA);
+	assert (suono_esplosione_navicella_misteriosa);
+	istanza_esplosione_navicella_misteriosa = al_create_sample_instance (suono_esplosione_navicella_misteriosa);
+	assert (istanza_esplosione_navicella_misteriosa);
+	al_attach_sample_instance_to_mixer(istanza_esplosione_navicella_misteriosa, al_get_default_mixer());
+
+	suono_sparo_carro_armato = al_load_sample (FILE_SUONO_SPARO_CARRO_ARMATO);
+	assert (suono_sparo_carro_armato);
+	istanza_sparo_carro_armato = al_create_sample_instance (suono_sparo_carro_armato);
+	assert (istanza_sparo_carro_armato);
+	al_attach_sample_instance_to_mixer(istanza_sparo_carro_armato, al_get_default_mixer());
+
+	suono_navicella_misteriosa = al_load_sample (FILE_SUONO_NAVICELLA_MISTERIOSA);
+	assert (suono_navicella_misteriosa);
+	istanza_navicella_misteriosa = al_create_sample_instance (suono_navicella_misteriosa);
+	assert (istanza_navicella_misteriosa);
+	al_attach_sample_instance_to_mixer(istanza_navicella_misteriosa, al_get_default_mixer());
+	al_set_sample_instance_playmode(istanza_navicella_misteriosa, ALLEGRO_PLAYMODE_LOOP);
 }
 
 void modificaVelocitaMusicaOndata (unsigned int percentuale_velocita_ondata)
--- gestione_audio.h
+++ gestione_audio.h
@@ -1,47 +1,94 @@
 /*
- * Header file contenente l'interfaccia del modulo di gestione dell'audio.
+ * Header file contenente l'interfaccia del modulo per la gestione dell'audio.
  */
 
 //INIZIO COSTANTI PER VELOCITA' AUDIO
-const float VELOCITA_SOTTOFONDO_ONDATA_MIN = 0.8;
-const float VELOCITA_SOTTOFONDO_ONDATA_MAX = 1.55;
+const float VELOCITA_SOTTOFONDO_ONDATA_MIN = 0.8; /**<Velocità minima utilizzata per la musica di sottofondo all'ondata aliena in movimento.*/
+const float VELOCITA_SOTTOFONDO_ONDATA_MAX = 1.55; /**<Velocità massima utilizzata per la musica di sottofondo all'ondata aliena in movimento.*/
 //FINE COSTANTI PER VELOCITA AUDIO
 
 //INIZIO COSTANTI PER FILE
 const char FILE_MUSICA_PRINCIPALE [] = "Sounds/principale.wav"; /**<Percorso del file contenente la musica principale.*/
-const char FILE_MUSICA_SOTTOFONDO_ONDATA [] = "Sounds/sottofondo_ondata.wav";
-const char FILE_SUONO_ESPLOSIONE_CARRO_ARMATO [] = "Sounds/esplosione_carro_armato.wav";
-const char FILE_SUONO_ESPLOSIONE_ALIENO [] = "Sounds/esplosione_alieno.wav";
-const char FILE_SUONO_ESPLOSIONE_NAVICELLA_MISTERIOSA [] = "Sounds/esplosione_navicella_misteriosa.wav";
-const char FILE_SUONO_SPARO_CARRO_ARMATO [] = "Sounds/sparo_carro_armato.wav";
-const char FILE_SUONO_NAVICELLA_MISTERIOSA [] = "Sounds/navicella_misteriosa.wav";
+const char FILE_MUSICA_SOTTOFONDO_ONDATA [] = "Sounds/sottofondo_ondata.wav"; /**<Percorso del file contenente la musica di sottofondo all'ondata aliena in movimento.*/
+const char FILE_SUONO_ESPLOSIONE_CARRO_ARMATO [] = "Sounds/esplosione_carro_armato.wav"; /**<Percorso del file contenente il suono dell'esplosione del carro armato.*/
+const char FILE_SUONO_ESPLOSIONE_ALIENO [] = "Sounds/esplosione_alieno.wav"; /**<Percorso del file contenente il suono dell'esplosione di un alieno.*/
+const char FILE_SUONO_ESPLOSIONE_NAVICELLA_MISTERIOSA [] = "Sounds/esplosione_navicella_misteriosa.wav"; /**<Percorso del file contenente il suono dell'esplosione della navicella misteriosa.*/
+const char FILE_SUONO_SPARO_CARRO_ARMATO [] = "Sounds/sparo_carro_armato.wav"; /**<Percorso del file contenente il suono dello sparo del carro armato.*/
+const char FILE_SUONO_NAVICELLA_MISTERIOSA [] = "Sounds/navicella_misteriosa.wav"; /**<Percorso del file contenente il suono della navicella misteriosa in movimento.*/
 //FINE COSTANTI PER FILE
 
 //INIZIO INTERFACCIA
-
-void inizializzaAudio ();
-
-void distruggiAudio ();
-
-void avviaMusicaPrincipale ();
-
-void fermaMusicaPrincipale ();
-
+/**
+ * Avvia la musica di sottofondo all'ondata di alieni in movimento.
+ */
 void avviaMusicaOndata ();
 
-void fermaMusicaOndata ();
+/**
+ * Avvia la musica principale.
+ */
+void avviaMusicaPrincipale ();
 
-void avviaSuonoNavicellaMisteriosa ();
-
-void fermaSuonoNavicellaMisteriosa ();
-
-void avviaSuonoSparoCarroArmato ();
-
-void avviaSuonoEsplosioneCarroArmato ();
-
+/**
+ * Esegue il suono dell'esplosione di un alieno.
+ */
 void avviaSuonoEsplosioneAlieno ();
 
+/**
+ * Esegue il suono dell'esplosione del carro armato.
+ */
+void avviaSuonoEsplosioneCarroArmato ();
+
+/**
+ * Esegue il suono dell'esplosione della navicella misteriosa.
+ */
 void avviaSuonoEsplosioneNavicellaMisteriosa ();
 
+/**
+ * Avvia il suono della navicella misteriosa in movimento.
+ */
+void avviaSuonoNavicellaMisteriosa ();
+
+/**
+ * Esegue il suono dello sparo del carro armato.
+ */
+void avviaSuonoSparoCarroArmato ();
+
+/**
+ * Procedura di distruzione del modulo per la gestione dell'audio.
+ * 
+ * Si occupa di deallocare correttamente la memoria alloccata in fase di inizializzazione del modulo.
+ */
+void distruggiAudio ();
+
+/**
+ * Ferma la musica di sottofondo all'ondata di alieni in movimento.
+ */
+void fermaMusicaOndata ();
+
+/**
+ * Ferma la musica principale.
+ */
+void fermaMusicaPrincipale ();
+
+/**
+ * Ferma il suono della navicella misteriosa in movimento.
+ */
+void fermaSuonoNavicellaMisteriosa ();
+
+/**
+ * Procedura di inizializzazione del modulo per la gestione dell'audio.
+ * 
+ * Si occupa di inizializzare correttamente tutte le variabili necessarie e di seguire i metodi di inizializzazione di determinate librerie "Allegro".
+ */
+void inizializzaAudio ();
+
+/**
+ * Modifica la velocità della musica di sottofondo all'ondata di alieni in movimento.
+ *
+ * La modifica viene effettuata sulla base della {@link VELOCITA_SOTTOFONDO_ONDATA_MIN velocità minima}
+ * e della {@link VELOCITA_SOTTOFONDO_ONDATA_MAX velocità massima} indicate.
+ *
+ * @param percentuale_velocita_ondata Percentuale della velocità che si vuole impostare.
+ */
 void modificaVelocitaMusicaOndata (unsigned int percentuale_velocita_ondata);
 //FINE INTERFACCIA
